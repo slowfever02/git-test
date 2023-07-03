1d3193b6f9db56ae3b90039fd5245f34c1de2e73
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 <slowfever@hth.com>
2023-07-03 10:04:11 +0800

    第二次提交

--- src/main/java/com/hth/git/GitTest.java
+++ src/main/java/com/hth/git/GitTest.java
@@ -3,5 +3,6 @@ package com.hth.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("helle git");
+        System.out.println("helle git2");
     }
 }
