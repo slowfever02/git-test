75fde5cf9e557145187abc9ea9098da6c72d9c0e
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 <slowfever@hth.com>
2023-07-03 10:41:58 +0800

    push commit

--- src/main/java/com/hth/git/GitTest.java
+++ src/main/java/com/hth/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("helle git4");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push commit");
     }
 }
